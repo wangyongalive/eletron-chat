a84bec48d90d4bbf37de46ba9301a2e62c02df2d
wangyongalive <jsonwang0102@163.com>
2025-05-12 20:17:28 +0800

    使用Iconify作为图标解决方案

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="flex items-center justify-between h-screen">
     <div class="bg-gray-200 text-gray-700 w-[300px] h-full border-r border-gray-300">
-      <ConversationList :items="items" />
+      <div class="h-[90%] overflow-y-auto">
+        <ConversationList :items="items" />
+      </div>
+      <div class="h-[10%] grid grid-cols-2 gap-2 p-2">
+        <button class="shadow-sm inline-flex items-center justify-center bg-green-700 text-white hover:bg-green-700/90
+        border border-green-700 h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]">
+          <Icon icon="radix-icons:chat-bubble" class="mr-2"></Icon>
+          新建聊天
+        </button>
+        <button class="shadow-sm inline-flex items-center justify-center bg-green-50 text-green-700 hover:bg-green-700
+        border border-green-700 h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]">
+          <Icon icon="radix-icons:gear" class="mr-2"></Icon>
+          应用设置
+        </button>
+      </div>
     </div>
     <div class="h-full flex-1">content</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Icon } from "@iconify/vue";
 import ConversationList from "./compontents/ConversationList.vue";
 import { ConversationProps } from "./types";
 

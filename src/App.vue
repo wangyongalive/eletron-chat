fd1345a82d559c2d3286b024cc397c879a3368a1
wangyongalive <jsonwang0102@163.com>
2025-05-12 17:56:24 +0800

    tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-    <h1>💖 Hello World!</h1>
-    <p>Welcome to your Electron application.</p>
+    <div class="flex items-center justify-between h-screen">
+        <div class="bg-gray-300 text-gray-700 w-[300px] h-full hover:bg-yellow-300">sidebar</div>
+        <div class="bg-green-300 text-blue-700 h-full flex-1">content
+            <input type="text" placeholder="Search" class="placeholder:text-blue-300" />
+        </div>
+    </div>
 </template>
 
 <script setup>
